72624b2222eb81a79840121b248df44e0aa780ab
OldOak <artxakrus@yandex.ru>
2026-02-12 22:29:25 +0400

    All tests have been modified using method sendHttpRequest()

--- src/test/ru/practicum/moviehub/http/MoviesApiTest.java
+++ src/test/ru/practicum/moviehub/http/MoviesApiTest.java
@@ -64,13 +64,9 @@ public class MoviesApiTest {
 
     @Test
     void getMovies_whenEmpty_returnsEmptyArray() throws Exception {
-        HttpRequest req = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/movies"))
-                .GET()
-                .build();
-
-        HttpResponse<String> resp =
-                client.send(req, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
+        URI uri = URI.create(BASE + "/movies");
+        HttpResponse<String> resp = sendHttpRequest(uri, "GET", null,
+                "application/json; charset=UTF-8");
 
         assertEquals(200, resp.statusCode(), "GET /movies должен вернуть 200");
 
@@ -86,14 +82,10 @@ public class MoviesApiTest {
 
     @Test
     void postMovie_whenSuccess() throws Exception {
-        HttpRequest req = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/movies"))
-                .POST(HttpRequest.BodyPublishers.ofString("{\"title\": \"Название фильма\", \"year\": 2026}"))
-                .header("Content-Type", "application/json; charset=UTF-8")
-                .build();
-
-        HttpResponse<String> resp =
-                client.send(req, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
+        URI uri = URI.create(BASE + "/movies");
+        HttpResponse<String> resp = sendHttpRequest(uri, "POST",
+                "{\"title\": \"Название фильма\", \"year\": 2026}",
+                "application/json; charset=UTF-8");
 
         assertEquals(201, resp.statusCode(), "POST /movies должен вернуть 201");
 
@@ -110,28 +102,20 @@ public class MoviesApiTest {
 
     @Test
     void postMovie_whenErrorValidationByYear() throws Exception {
-        HttpRequest req = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/movies"))
-                .POST(HttpRequest.BodyPublishers.ofString("{\"title\": \"Название фильма\", \"year\": 2047}"))
-                .header("Content-Type", "application/json; charset=UTF-8")
-                .build();
-
-        HttpResponse<String> resp =
-                client.send(req, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
+        URI uri = URI.create(BASE + "/movies");
+        HttpResponse<String> resp = sendHttpRequest(uri, "POST",
+                "{\"title\": \"Название фильма\", \"year\": 2047}",
+                "application/json; charset=UTF-8");
 
         assertEquals(422, resp.statusCode(), "POST /movies со значением года 2047 должен вернуть 422");
     }
 
     @Test
     void postMovie_whenErrorValidationByName() throws Exception {
-        HttpRequest req = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/movies"))
-                .POST(HttpRequest.BodyPublishers.ofString("{\"title\": \"     \", \"year\": 2026}"))
-                .header("Content-Type", "application/json; charset=UTF-8")
-                .build();
-
-        HttpResponse<String> resp =
-                client.send(req, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
+        URI uri = URI.create(BASE + "/movies");
+        HttpResponse<String> resp = sendHttpRequest(uri, "POST",
+                "{\"title\": \"     \", \"year\": 2026}",
+                "application/json; charset=UTF-8");
 
         assertEquals(422, resp.statusCode(),
                 "POST /movies с пустой строкой вместо названия должен вернуть 422");
@@ -139,14 +123,10 @@ public class MoviesApiTest {
 
     @Test
     void postMovie_whenErrorContentTypeByHeader() throws Exception {
-        HttpRequest req = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/movies"))
-                .POST(HttpRequest.BodyPublishers.ofString("{\"title\": \"Название фильма\", \"year\": 2026}"))
-                .header("Content-Type", "application/json")
-                .build();
-
-        HttpResponse<String> resp =
-                client.send(req, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
+        URI uri = URI.create(BASE + "/movies");
+        HttpResponse<String> resp = sendHttpRequest(uri, "POST",
+                "{\"title\": \"Название фильма\", \"year\": 2026}",
+                "application/json");
 
         assertEquals(415, resp.statusCode(),
                 "POST /movies в header не указан формат charset=UTF-8, должен вернуть 415");
@@ -154,125 +134,98 @@ public class MoviesApiTest {
 
     @Test
     void getMovieById_whenSuccess() throws Exception {
-        HttpRequest req = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/movies"))
-                .POST(HttpRequest.BodyPublishers.ofString("{\"title\": \"Название фильма\", \"year\": 2026}"))
-                .header("Content-Type", "application/json; charset=UTF-8")
-                .build();
-
-        HttpResponse<String> resp =
-                client.send(req, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
+        URI uri = URI.create(BASE + "/movies");
+        HttpResponse<String> resp = sendHttpRequest(uri, "POST",
+                "{\"title\": \"Название фильма\", \"year\": 2026}",
+                "application/json; charset=UTF-8");
         assertEquals(201, resp.statusCode());
 
-        HttpRequest req2 = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/movies/1"))
-                .GET()
-                .build();
+        URI uri2 = URI.create(BASE + "/movies/1");
+        HttpResponse<String> resp2 = sendHttpRequest(uri2, "GET",
+                null,
+                "application/json; charset=UTF-8");
 
-        HttpResponse<String> resp2 =
-                client.send(req2, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
         assertEquals(200, resp2.statusCode());
     }
 
     @Test
     void getMovieById_whenErrorMovieNotFound() throws Exception {
-        HttpRequest req2 = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/movies/1"))
-                .GET()
-                .build();
+        URI uri2 = URI.create(BASE + "/movies/1");
+        HttpResponse<String> resp2 = sendHttpRequest(uri2, "GET",
+                null,
+                "application/json; charset=UTF-8");
 
-        HttpResponse<String> resp2 =
-                client.send(req2, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
         assertEquals(404, resp2.statusCode());
     }
 
     @Test
     void getMovieById_whenErrorIdNotNumber() throws Exception {
-        HttpRequest req2 = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/movies/edfaaw"))
-                .GET()
-                .build();
+        URI uri2 = URI.create(BASE + "/movies/edfaaw");
+        HttpResponse<String> resp2 = sendHttpRequest(uri2, "GET",
+                null,
+                "application/json; charset=UTF-8");
 
-        HttpResponse<String> resp2 =
-                client.send(req2, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
         assertEquals(400, resp2.statusCode());
     }
 
     @Test
     void deleteMovieById_whenSuccessDelete() throws Exception {
-        HttpRequest req = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/movies"))
-                .POST(HttpRequest.BodyPublishers.ofString("{\"title\": \"Название фильма\", \"year\": 2026}"))
-                .header("Content-Type", "application/json; charset=UTF-8")
-                .build();
+        URI uri = URI.create(BASE + "/movies");
+        HttpResponse<String> resp = sendHttpRequest(uri, "POST",
+                "{\"title\": \"Название фильма\", \"year\": 2026}",
+                "application/json; charset=UTF-8");
 
-        HttpResponse<String> resp =
-                client.send(req, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
         assertEquals(201, resp.statusCode());
 
-        HttpRequest req2 = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/movies/1"))
-                .DELETE()
-                .build();
+        URI uri2 = URI.create(BASE + "/movies/1");
+        HttpResponse<String> resp2 = sendHttpRequest(uri2, "DELETE",
+                null,
+                "application/json; charset=UTF-8");
 
-        HttpResponse<String> resp2 =
-                client.send(req2, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
         assertEquals(204, resp2.statusCode());
     }
 
     @Test
     void deleteMovieById_whenErrorMovieNotFound() throws Exception {
-        HttpRequest req2 = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/movies/1"))
-                .DELETE()
-                .build();
+        URI uri2 = URI.create(BASE + "/movies/1");
+        HttpResponse<String> resp2 = sendHttpRequest(uri2, "DELETE",
+                null,
+                "application/json; charset=UTF-8");
 
-        HttpResponse<String> resp2 =
-                client.send(req2, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
         assertEquals(404, resp2.statusCode());
     }
 
     @Test
     void getMoviesByYear_whenSuccessYear() throws Exception {
-        HttpRequest req = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/movies"))
-                .POST(HttpRequest.BodyPublishers.ofString("{\"title\": \"Название фильма\", \"year\": 2026}"))
-                .header("Content-Type", "application/json; charset=UTF-8")
-                .build();
+        URI uri = URI.create(BASE + "/movies");
+        HttpResponse<String> resp = sendHttpRequest(uri, "POST",
+                "{\"title\": \"Название фильма\", \"year\": 2026}",
+                "application/json; charset=UTF-8");
 
-        HttpResponse<String> resp =
-                client.send(req, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
         assertEquals(201, resp.statusCode());
 
-        HttpRequest req2 = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/movies?year=2026"))
-                .GET()
-                .build();
+        URI uri2 = URI.create(BASE + "/movies?year=2026");
+        HttpResponse<String> resp2 = sendHttpRequest(uri2, "GET",
+                null,
+                "application/json; charset=UTF-8");
 
-        HttpResponse<String> resp2 =
-                client.send(req2, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
         assertEquals(200, resp2.statusCode());
     }
 
     @Test
     void getMoviesByYear_whenError() throws Exception {
-        HttpRequest req = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/movies"))
-                .POST(HttpRequest.BodyPublishers.ofString("{\"title\": \"Название фильма\", \"year\": 2026}"))
-                .header("Content-Type", "application/json; charset=UTF-8")
-                .build();
+        URI uri = URI.create(BASE + "/movies");
+        HttpResponse<String> resp = sendHttpRequest(uri, "POST",
+                "{\"title\": \"Название фильма\", \"year\": 2026}",
+                "application/json; charset=UTF-8");
 
-        HttpResponse<String> resp =
-                client.send(req, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
         assertEquals(201, resp.statusCode());
 
-        HttpRequest req2 = HttpRequest.newBuilder()
-                .uri(URI.create(BASE + "/movies?yearse=2026"))
-                .GET()
-                .build();
+        URI uri2 = URI.create(BASE + "/movies?yearse=2026");
+        HttpResponse<String> resp2 = sendHttpRequest(uri2, "GET",
+                null,
+                "application/json; charset=UTF-8");
 
-        HttpResponse<String> resp2 =
-                client.send(req2, HttpResponse.BodyHandlers.ofString(StandardCharsets.UTF_8));
         assertEquals(400, resp2.statusCode());
     }
 }
